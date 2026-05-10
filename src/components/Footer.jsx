4c3c3ecb805f08d74474c837647a8f58b637fe95
jimelabidinetoure <jimeltoure@gmail.com>
2026-05-10 20:40:54 +0200

    fix : gestion d'erreur

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,15 +53,7 @@ function Footer() {
         <div className="footer-col">
           <h4 className="footer-col-title">Contact</h4>
           <ul className="footer-links">
-            <li>
-              <a href="mailto:jimeltoure@gmail.com">
-                <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" style={{marginRight:6,verticalAlign:'middle'}}>
-                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
-                  <polyline points="22,6 12,13 2,6"/>
-                </svg>
-                jimeltoure@gmail.com
-              </a>
-            </li>
+         
             <li>
               <a href="mailto:jimelabidinetoure@gmail.com">
                 <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" style={{marginRight:6,verticalAlign:'middle'}}>
